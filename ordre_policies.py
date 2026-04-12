0102f390464d18c04d06e28a9675bb09f21fadaa
celeste-hash <tiagoprlt92@gmail.com>
2026-04-12 12:35:21 +0200

    creation de main Titi, un fichier qui reprend toutes les fonctions que j'utiliserai pour faire un main

--- ordre_policies.py
+++ ordre_policies.py
@@ -1,27 +1,27 @@
 def ordre_policies(l_copie):
-    p1=ch(input("quel est la policy la plus importante parmis : diplomatic, medical, technical ?")) 
-    if p1==diplomatic : 
+    p1=input("quel est la policy la plus importante parmis : diplomatic, medical, technical ?") 
+    if p1=="diplomatic" : 
         for i in l_copie : 
             l_copie["diplomatic"]=l_copie["diplomatic"]*2
-    elif p1== medical : 
+    elif p1== "medical" : 
         for i in l_copie : 
             l_copie["medical"]=l_copie["medical"]*2
-    elif p1==technical :
+    elif p1=="technical" :
         for i in l_copie : 
             l_copie["technical"]=l_copie["technical"]*2
     else : 
         return("votre policy n'est pas bien orthographiée ou bien pas dans notre liste des priorités")
     
-    p2=ch(input("quel est la policy la plus importante parmis : diplomatic, medical, technical ?")) 
+    p2=input("quel est la policy la plus importante parmis : diplomatic, medical, technical ?")
     if p1==p2 : 
         return("vous ne pouvez pas avoir la même policy en 1 et 2")
-    elif p2== medical : 
+    elif p2== "medical" : 
         for i in l_copie : 
             l_copie["medical"]=l_copie["medical"]*1,5
-    elif p2==technical :
+    elif p2=="technical" :
         for i in l_copie : 
             l_copie["technical"]=l_copie["technical"]*1,5
-    elif p1==diplomatic : 
+    elif p1=="diplomatic" : 
         for i in l_copie : 
             l_copie["diplomatic"]=l_copie["diplomatic"]*1,5
     else : 
